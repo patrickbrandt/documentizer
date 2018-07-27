49696f500e80783c3ea3854f15974b1fcb5e48fb
Patrick Brandt <pbrandt@coca-cola.com>
2018-07-27 07:03:40 -0700

    adding npm link for shared module to db and documentize

--- db/init.sh
+++ db/init.sh
@@ -1,2 +1,5 @@
-
+cd ../shared
+npm link
+cd ../db
+npm link shared
 npm prune . && npm install . && node make_tables.js
--- documentize/run.sh
+++ documentize/run.sh
@@ -1,1 +1,5 @@
+cd ../shared
+npm link
+cd ../documentize
+npm link shared
 npm prune . && npm install . && npm run documentize
