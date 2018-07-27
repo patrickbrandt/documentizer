6e6cc68e878aaa709c2fef11f309c3e2ae564d62
Patrick Brandt <pbrandt@coca-cola.com>
2018-07-27 12:02:19 -0700

    article api is functioning

--- api/api.js
+++ api/api.js
@@ -1,11 +1,35 @@
 const restify = require('restify');
+const {Aws} = require('../shared');
+
 const server = restify.createServer();
+const aws = new Aws(process.env.DYNAMODB_ENDPOINT);
+const dynamodb = aws.dynamodb;
+const doc = aws.doc;
 
 server.listen(8080, () => {
   console.log('ready on %s', server.url);
 });
 
-server.get('/', function (req, res, next) {
-   res.send({ hello: 'world' });
-   next();
+server.get('/', (req, res, next) => {
+  res.send({ hello: 'world' });
+  next();
+});
+
+server.get('/article', async (req, res, next) => {
+  const articles = await doc.scan({ TableName: 'articleDoc' }).promise();
+  res.send(articles.Items);
+  next();
+});
+
+server.get('/article/:id', async (req, res, next) => {
+  //console.log(JSON.stringify(req));
+  const params = {
+    TableName: 'articleDoc',
+    Key: {
+      id: parseInt(req.params.id),
+    },
+  };
+  const data = await doc.get(params).promise();  
+  res.send(data.Item);
+  next();
 });
